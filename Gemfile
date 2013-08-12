bd5a99593dade20f1614f6307fc8867a0c0dde06
asnow73 <albertulgu@list.ru>
2013-08-12 12:16:22 +0000

    Travis-CI

--- Gemfile
+++ Gemfile
@@ -32,11 +32,16 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+group :test do
+  gem "coveralls", require: false
+  gem "factory_girl_rails", "~> 4.0"
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
 # Use unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano', group: :development
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
+if ENV['TRAVIS']
+  require 'coveralls'
+  Coveralls.wear!('rails')
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+FactoryGirl.reload
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -12,4 +19,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include FactoryGirl::Syntax::Methods
 end
